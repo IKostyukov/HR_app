8a8102fae7b80d93c7531c72152c26907da5b9e7
IKostyukov <igorkostukov@gmail.com>
2020-08-24 16:23:38 +0300

    ////

--- app/settings.py
+++ app/settings.py
@@ -9,10 +9,10 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-
+import dj_database_url    
 from django.urls import reverse_lazy
 import os
-import dj_database_url    
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
