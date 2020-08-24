56a1d3a7ff25db4aa22b6cf0860bd1b82595cc09
IKostyukov <igorkostukov@gmail.com>
2020-08-24 18:48:57 +0300

    добавил aljwed hosts

--- app/settings.py
+++ app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vast-waters-63560.herokuapp.com']
 
 
 # Application definition
